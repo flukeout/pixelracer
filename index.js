fe2461e4cfda8165c97c812a5625774868b70b0e
Luke Pacholski <luke.pacholski@gmail.com>
2015-11-23 22:32:39 -0800

    Undo!

--- index.js
+++ index.js
@@ -51,13 +51,10 @@ app.get('/single', function(req, res) {
 
 
 
-var port = env.PORT || 5000;
-var server = app.listen(port, function () {
+var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
 });
-
-console.log('Starting server on port ' + port);
 
 socket.server(server);
 
